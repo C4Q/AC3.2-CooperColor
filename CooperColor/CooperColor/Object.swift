3efa720b4205e191dbb7133349feefa570d64d94
Margaret E. Ikeda <margaretei@gmail.com>
2016-11-08 12:29:43 -0500

    first commit 11/8

---
+++ CooperColor/CooperColor/Object.swift
@@ -0,0 +1,14 @@
+//
+//  Object.swift
+//  CooperColor
+//
+//  Created by Margaret Ikeda on 11/7/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import Foundation
+
+class Object {
+    let objectId: String
+    
+}
---
+++ CooperColor/CooperColor/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CooperColor
+//
+//  Created by Margaret Ikeda on 11/7/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cooperObjectImage: UIImageView!
+    @IBOutlet weak var cooperTombstoneLabel: UILabel!
+    
+    @IBOutlet weak var topBgColorLabel: UILabel!
+    @IBOutlet weak var middleBgColorLabel: UILabel!
+    @IBOutlet weak var bottomBgColorLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
